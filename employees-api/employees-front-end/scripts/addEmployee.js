d196a02cba8a5920be19961e3bed2d6e0641aa45
mariliacristina <mariliacristinacarmo@gmail.com>
2020-11-14 16:04:46 -0300

    Adding comments on addEmployee.js

--- employees-api/employees-front-end/scripts/addEmployee.js
+++ employees-api/employees-front-end/scripts/addEmployee.js
@@ -1,23 +1,25 @@
 const addForm = document.getElementById("add-form");
 
+// when submitting the insertion form, calls the post function
 addForm.addEventListener("submit", function (event) {
-    const name = document.getElementById("name").value;
-    const cpf = document.getElementById("cpf").value;
-    const position = document.getElementById("position").value;
-    const date = document.getElementById("date").value;
-    const uf = document.getElementById("uf").value;
-    const salary = document.getElementById("salary").value;
-    const status = document.getElementById("status").value;
+  const name = document.getElementById("name").value;
+  const cpf = document.getElementById("cpf").value;
+  const position = document.getElementById("position").value;
+  const date = document.getElementById("date").value;
+  const uf = document.getElementById("uf").value;
+  const salary = document.getElementById("salary").value;
+  const status = document.getElementById("status").value;
 
-    const employee = { name, cpf, position, date, uf, salary, status };
-    
-    postEmployee(employee);
-  
-    event.preventDefault();
-  });
+  const employee = { name, cpf, position, date, uf, salary, status };
 
-  function postEmployee(employee) {
-    axios.post('http://localhost:3000/api/employees/add', employee)
+  postEmployee(employee);
+
+  event.preventDefault();
+});
+
+function postEmployee(employee) {
+  axios
+    .post("http://localhost:3000/api/employees/add", employee)
     .then((response) => alert(response.data.status))
     .catch((error) => console.log(error));
-  }
+}
