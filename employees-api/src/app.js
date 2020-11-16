830a22fc2b738250306970788a44aea4343a69cc
mariliacristina <mariliacristinacarmo@gmail.com>
2020-11-16 14:24:22 -0300

    Adding the condition that the server does not listen on port 3000 if the process running is swagger.

--- employees-api/src/app.js
+++ employees-api/src/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // server listening  on port 3000
-if (process.env.NODE_ENV !== "test") {
+if (process.env.NODE_ENV !== "test" && process.env.NODE_ENV !== "swagger") {
   app.listen(3000);
 }
 
@@ -67,7 +67,7 @@ app.post("/api/employees/post", (req, res) => {
   const employee = req.body;
   const msg = addEmployee(employee);
 
-  // #swagger.responses[201] = { description: 'Employee added successfully!' }
+  // #swagger.responses[201] = { description: 'Employee added or updated successfully!' }
   res.status(201).json({ status: msg });
 });
 
